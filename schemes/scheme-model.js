8f6fcf46887ebd31304bf971fa01b1b6aa2fc3ea
robpedersendev <rob84044@gmail.com>
2019-08-13 20:43:40 -0600

    Added the update function in schemes/scheme-model.js file

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -17,3 +17,9 @@ function findSteps(id) {
 function add({ scheme_name }) {
     return db('schemes').insert({ scheme_name });
 }
+
+function update({ scheme_name }, id) {
+    return db('schemes')
+        .where({ id })
+        .update({ scheme_name })
+}
